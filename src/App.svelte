48380efcd64bd427c2e9a2f33f62c7547bb355ce
Antonio A R <antonio.axelsson@gmail.com>
2023-05-22 23:11:12 +0200

    Implement font size calculator

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,62 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	let defaultFontSize: number = 16;
+	let minViewportWidth: number = 320;
+	let maxViewportWidth: number = 1280;
+	let minFontSize: number = 1;
+	let maxFontSize: number = 2;
+
+	$: minViewportWidthInRem = minViewportWidth / defaultFontSize;
+	$: maxViewportWidthInRem = maxViewportWidth / defaultFontSize;
+
+	$: slope = (maxFontSize - minFontSize) / (maxViewportWidthInRem - minViewportWidthInRem);
+	$: yAxisIntersection = -minViewportWidthInRem * slope + minFontSize;
+
+	$: clampString = `clamp(${minFontSize}rem, ${yAxisIntersection.toFixed(4)}rem + ${(
+		slope * 100
+	).toFixed(4)}vw, ${maxFontSize}rem);`;
 </script>
 
+<header>
+	<h1>Font Clamper</h1>
+	<p>Calculate linearly scaling font size based on viewport width</p>
+</header>
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+	<section class="caluculator flex">
+		<label
+			>Default font size in pixels:
+			<input type="number" min="1" bind:value={defaultFontSize} />
+		</label>
+		<label
+			>Minimum viewport width in pixels:
+			<input type="number" min="1" bind:value={minViewportWidth} />
+		</label>
+		<label
+			>Maximum viewport width in pixels:
+			<input type="number" min="1" bind:value={maxViewportWidth} />
+		</label>
+		<label
+			>Minimum font size in rem:
+			<input type="number" min="1" bind:value={minFontSize} />
+		</label>
+		<label
+			>Maximum font size in rem:
+			<input type="number" min="1" bind:value={maxFontSize} />
+		</label>
+	</section>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<section class="result">
+		<code>{clampString}</code>
+	</section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	.flex {
+		display: flex;
+		flex-direction: column;
+	}
+
+	label {
+		margin-bottom: 1rem;
+	}
 </style>
